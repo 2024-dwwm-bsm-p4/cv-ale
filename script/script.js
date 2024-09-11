50e0d75052fe932d33ea670c8e39fa0fdd5419a3
Aledorian <aledorianlegrand@gmail.com>
2024-09-11 11:06:22 +0200

    photo ajouté format desktop

--- script/script.js
+++ script/script.js
@@ -1,17 +1,30 @@
-var clickTarget = document.querySelector(".skill_cont h2");
-var skills = document.querySelector(".skills")
+if (window.innerWidth > 1223) {
 
-console.log(clickTarget)
+const clickTarget = document.querySelector(".skill_cont h4")
+const skills = document.querySelector(".qualities")
+const header = document.querySelector(".header")
+
 
 clickTarget.addEventListener("click", function(e){
-    if (skills.id === "skills")(
-        document.getElementById("skills").id="skills_show",
-        console.log(skills.id)
+    if (skills.id === "qualities")(
+        document.getElementById("qualities").id="qualities_show"
     )
     else(
-        document.getElementById("skills_show").id="skills"
+        document.getElementById("qualities_show").id="qualities"
     )
 }
+),
 
-);
+clickTarget.addEventListener("mouseover", function(e){
+    clickTarget.setAttribute("style", "cursor: pointer")
+}
+)
 
+const photo = document.createElement("img")
+photo.setAttribute("src", "https://picsum.photos/200/200")
+photo.setAttribute("style", "border-radius: 50%")
+header.prepend(photo)
+
+let headerContent = header.innerHTML
+
+}
