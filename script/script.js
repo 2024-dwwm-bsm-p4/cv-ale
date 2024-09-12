939293d835347a127ec567f72c06d67eec2181b4
Aledorian <aledorianlegrand@gmail.com>
2024-09-12 13:46:32 +0200

    start redesign header + footer

--- script/script.js
+++ script/script.js
@@ -1,35 +1,18 @@
+let script = document.querySelector("script")
+
 if (window.innerWidth > 1223) {
 
 const clickTarget = document.querySelector(".skill_cont h4")
 const technosTarget = document.querySelector("#technos_title")
 const skills = document.querySelector(".qualities")
 const technos = document.querySelector(".technos")
-const header = document.querySelector(".header")
-const switchButton = document.createElement("ion-icon")
-const photo = document.createElement("img")
-let html = document.getElementsByTagName("html")
-
-
-
-// CREATION BUTTON SWITCH LIGH/DARK
-
-switchButton.setAttribute("name", "contrast-outline")
-switchButton.setAttribute("class", "visual_mode")
-header.append(switchButton)
-
-
-//CREATION PHOTO DESKTOP MODE
-photo.setAttribute("src", "https://picsum.photos/75/75")
-photo.setAttribute("style", "border-radius: 50%")
-photo.setAttribute("alt", "photo de profil")
-header.prepend(photo)
+const html = document.getElementsByTagName("html")
 
 // AJOUT FUNCTION ONCLICK DARK/LIGHT MODE
 const visualMode = document.querySelector(".visual_mode")
 
 visualMode.addEventListener("click", function(e){
     html[0].classList.toggle("dark_mode")
-    console.log(html);
 })
 
 visualMode.addEventListener("mouseover", function(e){
@@ -64,4 +47,9 @@ clickTarget.addEventListener("mouseover", function(e){
     clickTarget.setAttribute("style", "cursor: pointer")
 })
 
+}
+else{
+    window.addEventListener("resize", (e) => {
+        location.reload()
+    })
 }
